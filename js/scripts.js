4d0d993c41116a581ef1073779203b3942b95c83
paulomaia20 <paulo_maia20@live.com>
2018-07-12 22:33:03 +0100

    new

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,24 @@ function helpMe() {
     console.log(query)
     key="https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/ff9d6f82-6c16-4a51-91ce-da997da73754?subscription-key=bda2aef0e0b84729bbc03dfecb2b4b45&verbose=true&timezoneOffset=0&q= "
     window.location.replace(key+query);
+
+
+
+}
+
+function getResult()
+
+{
+    var _query = document.querySelector('pre').innerHTML;
+    console.log(_query);
+    var intents = JSON.parse(_query).intents;
+    var topIntent=intents[0].intent;
+    var topScore=intents[0].score; 
+
+    if (topScore>0.8)
+     window.location.replace("invoice.html");
+     else
+     window.location.replace("noresult.html");
+
+
 }
