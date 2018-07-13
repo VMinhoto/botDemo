42a743bf8804987383e2a4ce44d7cef30aedc816
paulomaia20 <paulo_maia20@live.com>
2018-07-13 04:43:53 +0100

    new

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,10 @@ function helpMe() {
     query=document.getElementById("message").value;
     console.log(query)
     key="https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/ff9d6f82-6c16-4a51-91ce-da997da73754?subscription-key=bda2aef0e0b84729bbc03dfecb2b4b45&verbose=true&timezoneOffset=0&q="
-    $.getJSON(query+key, function(data) {
+    $.getJSON(key+query, function(data) {
         console.log("entered request")
         getResult(data)
+		console.log("data is");
         console.log(data)}
     )
 };
@@ -17,11 +18,11 @@ function getResult(data)
 {
     var _query = data;
     console.log(_query);
-    var intents = JSON.parse(_query).intents;
+    var intents = _query.intents;
     var topIntent=intents[0].intent;
     var topScore=intents[0].score; 
 
-    if (topScore>0.8)
+    if (topScore>0.8&&topIntent=='createInvoice')
      {
          window.location.replace("https://vminhoto.github.io/botDemo/invoice.html");
      }
